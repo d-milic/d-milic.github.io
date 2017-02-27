76350b69a66a2270fd746e31b184442fddb4de8b
Dušan Milić <milicdusan89@gmail.com>
2017-02-27 17:29:31 +0100

    Added changing of background on mouse hover over icons

--- scripts/application.js
+++ scripts/application.js
@@ -1,5 +1,17 @@
-$(document).ready() {
-  $('#upwork').mouseover(function(){
-      $(document.body).css('background-color', '#5BBC2E')
-  })
-}
+$(document).ready(function($) {
+
+  $('#upwork a').hover(
+    function() { $(document.body).addClass('upwork-bg'); },
+    function() { $(document.body).removeClass('upwork-bg'); }
+  );
+
+  $('#github a').hover(
+    function() { $(document.body).addClass('github-bg'); },
+    function() { $(document.body).removeClass('github-bg'); }
+  );
+
+  $('#linkedin a').hover(
+    function() { $(document.body).addClass('linkedin-bg'); },
+    function() { $(document.body).removeClass('linkedin-bg'); }
+  );
+})
